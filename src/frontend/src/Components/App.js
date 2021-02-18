ac24b1727499d3ab614ad2ba8cb1c5002f42b4c3
Anusha M <anusha.mantappanavar@gmail.com>
2021-02-18 21:18:33 +0530

    NavBar final changes

--- src/frontend/src/Components/App.js
+++ src/frontend/src/Components/App.js
@@ -1,44 +1,9 @@
 import './App.css';
-import {NavItem, Navbar, Icon} from 'react-materialize';
+import NavBar from './NavBar/NavBar'
+
 function App() {
   return (
-    <div className="App">
-      <div className="container-fluid">
-        <div className="row">
-        <Navbar
-            style={{backgroundColor:'#1976D2'}}
-            alignLinks="right"
-            brand={<a className="brand-logo" href="#">Tasks</a>}
-            id="mobile-nav"
-            menuIcon={<Icon>menu</Icon>}
-            options={{
-              draggable: true,
-              edge: 'left',
-              inDuration: 250,
-              onCloseEnd: null,
-              onCloseStart: null,
-              onOpenEnd: null,
-              onOpenStart: null,
-              outDuration: 200,
-              preventScrolling: true
-            }}
-            sidenav={
-              <ul style={{textAlign: "center"}}>
-                <li><a href="#">Login</a></li>
-                <li><a href="#">Signup</a></li>
-              </ul>
-              }
-            >
-            <NavItem href="" onClick={e => e.preventDefault()}>
-              Login
-            </NavItem>
-            <NavItem href="" onClick={e => e.preventDefault()}>
-              Sign up
-            </NavItem>
-          </Navbar>
-        </div>
-      </div>
-    </div>
+    <NavBar/>
   );
 }
 
--- src/frontend/src/Components/NavBar/NavBar.js
+++ src/frontend/src/Components/NavBar/NavBar.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import { NavItem, Navbar, Icon } from 'react-materialize';
+
+
 
 const NavBar = () => {
-    return (<div>
-        
-    </div>  );
+    return ( <div className="App">
+    <div className="container-fluid">
+      <div className="row">
+      <Navbar
+          style={{backgroundColor:'#1976D2'}}
+          alignLinks="right"
+          brand={<a className="brand-logo" href="#">Tasks</a>}
+          id="mobile-nav"
+          menuIcon={<Icon>menu</Icon>}
+          options={{
+            draggable: true,
+            edge: 'left',
+            inDuration: 250,
+            onCloseEnd: null,
+            onCloseStart: null,
+            onOpenEnd: null,
+            onOpenStart: null,
+            outDuration: 200,
+            preventScrolling: true
+          }}
+          sidenav={
+            <ul style={{textAlign: "center"}}>
+              <li><a href="#">Login</a></li>
+              <li><a href="#">Signup</a></li>
+            </ul>
+            }
+          >
+          <NavItem href="" onClick={e => e.preventDefault()}>
+            Login
+          </NavItem>
+          <NavItem href="" onClick={e => e.preventDefault()}>
+            Sign up
+          </NavItem>
+        </Navbar>
+      </div>
+    </div>
+  </div>  );
 }
  
 export default NavBar;
