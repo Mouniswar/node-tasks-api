ad74a0df772805b73070e8bd00cc043df130bf29
Anusha M <anusha.mantappanavar@gmail.com>
2021-02-18 15:51:49 +0530

    Added nav-bar

---
+++ src/frontend/src/Components/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container-fluid">
+        <div className="row">
+        <nav>
+         <div className="nav-wrapper blue darken-1">
+              <a href="#" class="brand-logo">Logo</a>
+           <ul id="nav-mobile" class="right hide-on-med-and-down">
+             <li><a href="sass.html">SignUp</a></li>
+             <li><a href="badges.html">LogIn</a></li>
+           </ul>
+         </div>
+        </nav>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
